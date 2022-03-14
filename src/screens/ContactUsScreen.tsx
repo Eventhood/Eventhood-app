b3960676f0c8e20e2e308b2eb7cfa91f30779618
Kevan-Y <58233223+Kevan-Y@users.noreply.github.com>
2022-03-14 16:20:13 -0400

    Link support screen with API

--- src/screens/ContactUsScreen.tsx
+++ src/screens/ContactUsScreen.tsx
@@ -1,4 +1,4 @@
-import { View, Platform, TouchableWithoutFeedback, Keyboard, StyleSheet } from 'react-native';
+import { View, TouchableWithoutFeedback, Keyboard, StyleSheet } from 'react-native';
 import {
   Select,
   Box,
@@ -8,27 +8,49 @@ import {
   Button,
   TextArea,
   Text,
-  KeyboardAvoidingView,
 } from 'native-base';
 import { useState, useEffect } from 'react';
+import { getAuth } from 'firebase/auth';
+
+import { app } from '../utils/firebase';
 import { URL } from '@env';
+
 interface Topic {
   _id: string;
   name: string;
 }
 
-const ContactUsScreen = () => {
+const auth = getAuth(app);
+
+const ContactUsScreen = ({ navigation }: any) => {
   const [topics, setTopics] = useState<Topic[] | undefined>();
   const [message, setMessage] = useState('');
   const [category, setCategory] = useState('');
   const [messageError, setMessageError] = useState(false);
   const [categoryError, setCategoryError] = useState(false);
 
-  const submitHandler = () => {
+  const submitHandler = async () => {
     setMessageError(message ? false : true);
     setCategoryError(category ? false : true);
 
-    if (message && category) console.warn(category, message);
+    if (message && category) {
+      const res = await fetch(`${URL}/api/users/${auth.currentUser?.uid}`);
+      const jsonRes = await res.json();
+
+      await fetch(`${URL}/api/contactrequests	`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          contactRequestData: {
+            user: jsonRes.data._id,
+            topic: category,
+            message: message,
+          },
+        }),
+      });
+    }
   };
 
   const fetchTopics = async () => {
@@ -43,61 +65,59 @@ const ContactUsScreen = () => {
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <KeyboardAvoidingView
-        h={{
-          base: '400px',
-          lg: 'auto',
-        }}
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      >
-        <View>
-          <FormControl isRequired isInvalid={categoryError} mt="8" px="4">
-            <FormControl.Label>
-              <Text fontSize="xl" fontWeight="semibold">
-                Category
-              </Text>
-            </FormControl.Label>
-            <Select
-              minWidth="200"
-              accessibilityLabel="Choose Category"
-              placeholder="Choose Category"
-              _selectedItem={{
-                bg: 'teal.600',
-                endIcon: <CheckIcon size={5} />,
-              }}
-              onValueChange={(category) => setCategory(category)}
-            >
-              {topics ? (
-                topics.map((topic, key) => {
-                  return <Select.Item label={topic.name} value={topic.name} key={key} />;
-                })
-              ) : (
-                <Select.Item label="No topic" value="No topic" key="No topic" />
-              )}
-            </Select>
-            <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
-              Please make a selection!
-            </FormControl.ErrorMessage>
-          </FormControl>
-          <FormControl isRequired isInvalid={messageError} mt="8" px="4" key={13}>
-            <FormControl.Label>
-              <Text fontSize="xl" fontWeight="semibold">
-                Message
-              </Text>
-            </FormControl.Label>
-            <TextArea h={40} placeholder="Your message" onChangeText={(text) => setMessage(text)} />
+      <View>
+        <FormControl isRequired isInvalid={categoryError} mt="8" px="4">
+          <FormControl.Label>
+            <Text fontSize="xl" fontWeight="semibold">
+              Category
+            </Text>
+          </FormControl.Label>
+          <Select
+            minWidth="200"
+            accessibilityLabel="Choose Category"
+            placeholder="Choose Category"
+            _selectedItem={{
+              bg: 'teal.600',
+              endIcon: <CheckIcon size={5} />,
+            }}
+            onValueChange={(category) => setCategory(category)}
+          >
+            {topics ? (
+              topics.map((topic, key) => {
+                return <Select.Item label={topic.name} value={topic._id} key={key} />;
+              })
+            ) : (
+              <Select.Item label="No topic" value="No topic" key="No topic" />
+            )}
+          </Select>
+          <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+            Please make a selection!
+          </FormControl.ErrorMessage>
+        </FormControl>
+        <FormControl isRequired isInvalid={messageError} mt="8" px="4" key={13}>
+          <FormControl.Label>
+            <Text fontSize="xl" fontWeight="semibold">
+              Message
+            </Text>
+          </FormControl.Label>
+          <TextArea h={40} placeholder="Your message" onChangeText={(text) => setMessage(text)} />
 
-            <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
-              Please write a message!
-            </FormControl.ErrorMessage>
-          </FormControl>
-          <Box px="4" mt="16">
-            <Button onPress={submitHandler} style={styles.button}>
-              Send
-            </Button>
-          </Box>
-        </View>
-      </KeyboardAvoidingView>
+          <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+            Please write a message!
+          </FormControl.ErrorMessage>
+        </FormControl>
+        <Box px="4" mt="16">
+          <Button
+            onPress={async () => {
+              await submitHandler();
+              navigation.goBack();
+            }}
+            style={styles.button}
+          >
+            Send
+          </Button>
+        </Box>
+      </View>
     </TouchableWithoutFeedback>
   );
 };
