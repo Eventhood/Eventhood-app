b29ecc8b20a7e8bb64316021d93600c444b6f9ec
Kevan-Y <58233223+Kevan-Y@users.noreply.github.com>
2022-03-17 22:42:39 -0400

    Change event card

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -8,42 +8,39 @@ const EventCard = ({ navigation }: any) => {
       underlayColor="#D1D5DB75"
       onPress={() => navigation.navigate('EventDetail', { id: 'MyID' })}
     >
-      <Box padding={5}>
-        <AspectRatio ratio={16 / 9}>
-          <Image
-            roundedTop="lg"
-            source={{
-              uri: 'https://images.unsplash.com/photo-1585409677983-0f6c41ca9c3b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=maxhttps://images.unsplash.com/photo-1585409677983-0f6c41ca9c3b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max',
-            }}
-            alt="image"
-          />
-        </AspectRatio>
-
-        <Stack p={4} space={2} backgroundColor="#ffffff" shadow={4}>
+      <Box padding={6}>
+        <Image
+          roundedTop="lg"
+          source={{
+            uri: 'https://images.unsplash.com/photo-1585409677983-0f6c41ca9c3b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=maxhttps://images.unsplash.com/photo-1585409677983-0f6c41ca9c3b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max',
+          }}
+          alt="image"
+          width="full"
+          height={150}
+        />
+        <Stack p={4} space={2} backgroundColor="#ffffff">
           <Stack space={10}>
             <HStack alignItems="center" space={4} justifyContent="space-between">
               <HStack alignItems="center">
-                <Heading size="md">Event title</Heading>
+                <Heading size="md">eventInfo.name</Heading>
               </HStack>
               <HStack alignItems="center">
                 <Text ml={1} color="gray.500" fontWeight="500">
-                  20Km
+                  20 Km
                 </Text>
               </HStack>
             </HStack>
           </Stack>
-          <Text>
-            Short Description. Lorem ipsum dolor sit amet, consectetur adipiscing elit ...
-          </Text>
+          <Text>eventInfo.description</Text>
           <HStack alignItems="center" space={4} justifyContent="space-between">
             <HStack alignItems="center">
               <Text ml={1} color="gray.500" fontWeight="500">
-                Created by @Username
+                Created by @ev
               </Text>
             </HStack>
             <HStack alignItems="center">
               <Text ml={1} color="gray.500" fontWeight="500">
-                Max Participants 8
+                Max Participants 1
               </Text>
             </HStack>
           </HStack>
