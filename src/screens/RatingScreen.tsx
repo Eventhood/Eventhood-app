e611459500fe1a0ce0d2f7e38071918c532311c0
Farshad Muhit Rashid <farsshad.rashid@gmail.com>
2022-03-28 12:53:05 -0400

    User Rating UI

---
+++ src/screens/RatingScreen.tsx
@@ -0,0 +1,264 @@
+import React from 'react';
+import { View, ScrollView, StyleSheet } from 'react-native';
+import { Text, Box, Center, Heading, HStack, Stack, Avatar, Button } from 'native-base';
+
+const RatingScreen = ({ navigation }: any) => {
+  return (
+    <View>
+      <ScrollView>
+        <View>
+          <Box paddingLeft={10} paddingRight={10} paddingBottom={6} paddingTop={6}>
+            <Stack p={2} borderTopRadius={30} backgroundColor="black" space={2}>
+              <Stack space={10}>
+                <HStack>
+                  <Avatar
+                    size="sm"
+                    source={{
+                      uri: 'https://pbs.twimg.com/profile_images/1309797238651060226/18cm6VhQ_400x400.jpg',
+                    }}
+                  >
+                    AK
+                  </Avatar>
+                  <Text ml={1} alignItems="center" color="white" fontSize="xl" fontWeight="500">
+                    @username
+                  </Text>
+                </HStack>
+              </Stack>
+            </Stack>
+            <Stack p={2} borderBottomRadius={30} backgroundColor="white" space={2}>
+              <Stack backgroundColor="white" alignItems="center">
+                <HStack>
+                  <Text fontSize="lg" color="black" fontWeight="500">
+                    +5 Rating
+                  </Text>
+                </HStack>
+              </Stack>
+            </Stack>
+          </Box>
+          <Box paddingLeft={10} paddingRight={10} paddingBottom={6} paddingTop={2}>
+            <Stack p={2} borderTopRadius={30} backgroundColor="black" space={2}>
+              <Stack space={10}>
+                <HStack>
+                  <Avatar
+                    size="sm"
+                    source={{
+                      uri: 'https://pbs.twimg.com/profile_images/1309797238651060226/18cm6VhQ_400x400.jpg',
+                    }}
+                  >
+                    AK
+                  </Avatar>
+                  <Text ml={1} alignItems="center" color="white" fontSize="xl" fontWeight="500">
+                    @username
+                  </Text>
+                </HStack>
+              </Stack>
+            </Stack>
+            <Stack p={2} borderBottomRadius={30} backgroundColor="white" space={2}>
+              <Stack backgroundColor="white" alignItems="center">
+                <HStack>
+                  <Text fontSize="lg" color="black" fontWeight="500">
+                    +5 Rating
+                  </Text>
+                </HStack>
+              </Stack>
+            </Stack>
+          </Box>
+
+          <Box paddingLeft={10} paddingRight={10} paddingBottom={6} paddingTop={2}>
+            <Stack p={2} borderTopRadius={30} backgroundColor="black" space={2}>
+              <Stack space={10}>
+                <HStack>
+                  <Avatar
+                    size="sm"
+                    source={{
+                      uri: 'https://pbs.twimg.com/profile_images/1309797238651060226/18cm6VhQ_400x400.jpg',
+                    }}
+                  >
+                    AK
+                  </Avatar>
+                  <Text ml={1} alignItems="center" color="white" fontSize="xl" fontWeight="500">
+                    @username
+                  </Text>
+                </HStack>
+              </Stack>
+            </Stack>
+            <Stack p={2} borderBottomRadius={30} backgroundColor="white" space={2}>
+              <Stack backgroundColor="white" alignItems="center">
+                <HStack>
+                  <Text fontSize="lg" color="black" fontWeight="500">
+                    +5 Rating
+                  </Text>
+                </HStack>
+              </Stack>
+            </Stack>
+          </Box>
+
+          <Box paddingLeft={10} paddingRight={10} paddingBottom={6} paddingTop={2}>
+            <Stack p={2} borderTopRadius={30} backgroundColor="black" space={2}>
+              <Stack space={10}>
+                <HStack>
+                  <Avatar
+                    size="sm"
+                    source={{
+                      uri: 'https://pbs.twimg.com/profile_images/1309797238651060226/18cm6VhQ_400x400.jpg',
+                    }}
+                  >
+                    AK
+                  </Avatar>
+                  <Text ml={1} alignItems="center" color="white" fontSize="xl" fontWeight="500">
+                    @username
+                  </Text>
+                </HStack>
+              </Stack>
+            </Stack>
+            <Stack p={2} borderBottomRadius={30} backgroundColor="white" space={2}>
+              <Stack backgroundColor="white" alignItems="center">
+                <HStack>
+                  <Text fontSize="lg" color="black" fontWeight="500">
+                    +5 Rating
+                  </Text>
+                </HStack>
+              </Stack>
+            </Stack>
+          </Box>
+
+          <Box paddingLeft={10} paddingRight={10} paddingBottom={6} paddingTop={2}>
+            <Stack p={2} borderTopRadius={30} backgroundColor="black" space={2}>
+              <Stack space={10}>
+                <HStack>
+                  <Avatar
+                    size="sm"
+                    source={{
+                      uri: 'https://pbs.twimg.com/profile_images/1309797238651060226/18cm6VhQ_400x400.jpg',
+                    }}
+                  >
+                    AK
+                  </Avatar>
+                  <Text ml={1} alignItems="center" color="white" fontSize="xl" fontWeight="500">
+                    @username
+                  </Text>
+                </HStack>
+              </Stack>
+            </Stack>
+            <Stack p={2} borderBottomRadius={30} backgroundColor="white" space={2}>
+              <Stack backgroundColor="white" alignItems="center">
+                <HStack>
+                  <Text fontSize="lg" color="black" fontWeight="500">
+                    +5 Rating
+                  </Text>
+                </HStack>
+              </Stack>
+            </Stack>
+          </Box>
+
+          <Box paddingLeft={10} paddingRight={10} paddingBottom={6} paddingTop={2}>
+            <Stack p={2} borderTopRadius={30} backgroundColor="black" space={2}>
+              <Stack space={10}>
+                <HStack>
+                  <Avatar
+                    size="sm"
+                    source={{
+                      uri: 'https://pbs.twimg.com/profile_images/1309797238651060226/18cm6VhQ_400x400.jpg',
+                    }}
+                  >
+                    AK
+                  </Avatar>
+                  <Text ml={1} alignItems="center" color="white" fontSize="xl" fontWeight="500">
+                    @username
+                  </Text>
+                </HStack>
+              </Stack>
+            </Stack>
+            <Stack p={2} borderBottomRadius={30} backgroundColor="white" space={2}>
+              <Stack backgroundColor="white" alignItems="center">
+                <HStack>
+                  <Text fontSize="lg" color="black" fontWeight="500">
+                    +5 Rating
+                  </Text>
+                </HStack>
+              </Stack>
+            </Stack>
+          </Box>
+          <Box paddingLeft={10} paddingRight={10} paddingBottom={6} paddingTop={2}>
+            <Stack p={2} borderTopRadius={30} backgroundColor="black" space={2}>
+              <Stack space={10}>
+                <HStack>
+                  <Avatar
+                    size="sm"
+                    source={{
+                      uri: 'https://pbs.twimg.com/profile_images/1309797238651060226/18cm6VhQ_400x400.jpg',
+                    }}
+                  >
+                    AK
+                  </Avatar>
+                  <Text ml={1} alignItems="center" color="white" fontSize="xl" fontWeight="500">
+                    @username
+                  </Text>
+                </HStack>
+              </Stack>
+            </Stack>
+            <Stack p={2} borderBottomRadius={30} backgroundColor="white" space={2}>
+              <Stack backgroundColor="white" alignItems="center">
+                <HStack>
+                  <Text fontSize="lg" color="black" fontWeight="500">
+                    +5 Rating
+                  </Text>
+                </HStack>
+              </Stack>
+            </Stack>
+          </Box>
+          <Box paddingLeft={10} paddingRight={10} paddingBottom={6} paddingTop={2}>
+            <Stack p={2} borderTopRadius={30} backgroundColor="black" space={2}>
+              <Stack space={10}>
+                <HStack>
+                  <Avatar
+                    size="sm"
+                    source={{
+                      uri: 'https://pbs.twimg.com/profile_images/1309797238651060226/18cm6VhQ_400x400.jpg',
+                    }}
+                  >
+                    AK
+                  </Avatar>
+                  <Text ml={1} alignItems="center" color="white" fontSize="xl" fontWeight="500">
+                    @username
+                  </Text>
+                </HStack>
+              </Stack>
+            </Stack>
+            <Stack p={2} borderBottomRadius={30} backgroundColor="white" space={2}>
+              <Stack backgroundColor="white" alignItems="center">
+                <HStack>
+                  <Text fontSize="lg" color="black" fontWeight="500">
+                    +5 Rating
+                  </Text>
+                </HStack>
+              </Stack>
+            </Stack>
+          </Box>
+        </View>
+      </ScrollView> 
+      <Button  onPress={() => navigation.navigate('RatingScrollScreen')} style={styles.buttonStyle}>
+        <Text style={styles.buttonTextStyle}>+</Text>
+      </Button>
+    </View>
+  );
+};
+
+export default RatingScreen;
+
+const styles = StyleSheet.create({
+  buttonStyle: {
+    backgroundColor: 'white',
+    width: 66,
+    height: 66,
+    borderRadius: 33,
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+  },
+  buttonTextStyle: {
+    color: 'black',
+    fontSize: 25,
+    marginTop: 7,
+  },
+});
